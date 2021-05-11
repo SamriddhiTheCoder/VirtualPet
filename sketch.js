40af24914b22a9a65d6f089820cfaded3f6037c9
SamriddhiTheCoder <77163721+SamriddhiTheCoder@users.noreply.github.com>
2021-05-11 21:07:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,9 @@
 var dogImg, dogImg2;
 var db;
 var foodS;
+var feed, add;
+var fedTime, lastFed;
+var foodObj;
 
 function preload(){
   dogImg = loadImage("images/dogImg.png");
@@ -8,46 +11,75 @@ function preload(){
 }
 
 function setup() {
-	createCanvas(500, 500);
+	createCanvas(800, 500);
   db = firebase.database();
   dbref = db.ref("food");
   dbref.on("value", readStock);
 
-  dog = createSprite(250, 250);
+  dog = createSprite(650, 250);
   dog.addImage(dogImg);
   dog.scale = 0.4;
+
+  foodObj = new Food(250, 250);
+
+  feed = createButton("Feed The Dog");
+  feed.position(550, 70);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("Add Food");
+  addFood.position(650, 70);
+  addFood.mousePressed(addFoods);
 }
 
 function draw() {  
   background(46, 139, 87);
 
-  if(keyWentUp("up")){
-    writeStocks(foodS);
-    dog.addImage(dogImg2);
-    dog.scale = 0.4;
+  fedTime = db.ref("feedTime");
+  fedTime.on("value", function(data){
+    lastFed=data.val();
+  });
+
+  push();
+  fill(255, 255, 254);
+  textSize(15);
+  if(lastFed>=12){
+    text("Last Feed: "+ lastFed % 12 + "PM", 600, 30);
   }
+  else if(lastFed==0){
+    text("Last Feed: 12 AM", 600, 30);
+  }
+  else{
+    text("Last Feed: "+ lastFed + "AM", 600, 30);
+  }
+
+  foodObj.display();
 
   drawSprites();
 
-  textSize(20);
+  textSize(15);
   fill("white");
-  text("Press up arrow to feed the dog!", 95, 20);
-  text("Food Left: "+foodS, 10, 80);
+  //text("Food Left: "+foodS, 10, 30);
 }
 
 function readStock(data){
   foodS = data.val();
+  foodObj.updateFoodStock(foodS);
 }
 
-function writeStocks(x){
-  if(x < 0){
-    x = 0;
-  }else{
-    x -= 1;
-  }
+function feedDog(){
+  dog.addImage(dogImg2);
 
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1);
   db.ref("/").update({
-    food : x
+    food : foodObj.getFoodStock(),
+    feedTime : hour()
+  });
+}
+
+function addFoods(){
+  foodS++;
+  db.ref("/").update({
+    food : foodS
   });
 }
 
